0bb015d27055245999731f5082abae0f66d349f6
07anuragxgit <anuragbnglr07@gmail.com>
2025-03-08 15:08:37 +0530

    detailed info of variables

--- day1.py
+++ day1.py
@@ -8,14 +8,36 @@
 #     c = addition(a,b)
 #     print(c)
 
-def addition(a,b, d): #function declaration
-    c = a + b + d #specifying the logic
-    return c # returning the value
+# def addition(a,b, d): #function declaration
+#     c = a + b + d #specifying the logic
+#     return c # returning the value
 
-res = addition(5,7, 10) #calling the function with the values
-print(res) #output
+# res = addition(5,7, 10) #calling the function with the values
+# print(res) #output
 
-print("HEllo world")
-div = 5/5 #interpreter throws the error here
-print("second hello world!!")
+# print("HEllo world")
+# div = 5/5 #interpreter throws the error here
+# print("second hello world!!")
 
+# name = "anurag" #declaring variables " double quotes refers to string"
+# x = 15 
+# age = 22 #int
+# weight = 45.6 #float
+# is_student = true #boolean 
+# #variables are case sensitive,there's diff btw age and Age
+# #always use underscore or letters while using variables 
+# #variable means a container which  stores data in a memory
+
+# x = 10 #updating a variable
+# x = x + 20 
+# print (x) # x value is updated to 30
+
+# a,b,c = 1,2,3 #assigning multiple values
+# print(a,b,c)
+
+# x,y = 5,10 #swapping variables
+# x,y = y,x
+# print (x,y)
+
+username = input("Enter your name: ")
+print("Hello,", username)
