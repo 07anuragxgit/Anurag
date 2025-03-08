bf8a35368cb87adf6979c48ef7a39a1950f0e907
07anuragxgit <anuragbnglr07@gmail.com>
2025-03-08 13:38:17 +0530

    variables,funtion declaration,diff btw compiler and interpreter,defining a function

--- day1.py
+++ day1.py
@@ -1,1 +1,21 @@
-print("hello world")
+# print("hello world")
+# print(5)
+
+# def addition(a,b):
+#     return a+b
+#     a = 9
+#     b = 7
+#     c = addition(a,b)
+#     print(c)
+
+def addition(a,b, d): #function declaration
+    c = a + b + d #specifying the logic
+    return c # returning the value
+
+res = addition(5,7, 10) #calling the function with the values
+print(res) #output
+
+print("HEllo world")
+div = 5/5 #interpreter throws the error here
+print("second hello world!!")
+
